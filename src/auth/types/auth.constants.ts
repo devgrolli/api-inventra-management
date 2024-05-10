e42d46b272542132a96b039050f30339cec04cab
lgdoliveira <lgdoliveira@rd.com.br>
2024-05-10 09:12:16 -0300

    ajuste de constants

--- src/auth/types/auth.constants.ts
+++ src/auth/types/auth.constants.ts
@@ -1,4 +1,4 @@
-export const SUCCESS_MESSAGES = {
+export const SUCCESS_MESSAGES = Object.freeze({
   PASSWORD_CHANGED: 'Senha alterada com sucesso!',
   CODE_RECOVERY_SUCCESS: 'Código de recuperação validado com sucesso.',
   CODE_SENT_EMAIL:
@@ -6,9 +6,9 @@ export const SUCCESS_MESSAGES = {
   USER_REGISTERED:
     'Cadastrado com sucesso, espere o administrador fazer a confirmação.',
   LOGIN_SUCCESS: 'Login realizado com sucesso',
-};
+});
 
-export const ERROR_MESSAGES = {
+export const ERROR_MESSAGES = Object.freeze({
   INVALID_TOKEN: 'Código inválido ou expirado.',
   UPDATE_NOTIFICATION: 'Ocorreu um erro ao atualizar campo disableNotify',
   UPDATE_ACCESS_USER: 'Ocorreu um erro ao atualizar a permissão do usuário',
@@ -17,18 +17,21 @@ export const ERROR_MESSAGES = {
   INVALID_CODE: 'Código de recuperação inválido.',
   CODE_EXPIRED: 'Código de recuperação expirou.',
   CPF_EXISTS: 'Esse CPF já existe.',
-  EMAIL_IN_USE: (email: string) =>
-    `Email ${email} já está em uso, tente outro email.`,
   EMAIL_NOT_FOUND: 'E-mail não encontrado.',
   EMAIL_SEND_ERROR: 'Ocorreu um erro ao enviar o e-mail. Tente novamente.',
   USER_NOT_FOUND: 'Usuário não encontrado',
   INVALID_PASSWORD: 'Senha inválida',
   AWAITING_ADMIN_APPROVAL: 'Aguarde a aprovação do administrador para entrar',
   INVALID_RECOVERY_CODE: 'Código de recuperação inválido.',
+});
+
+export const ERROR_MESSAGES_DYNAMIC = {
+  EMAIL_IN_USE: (email: string) =>
+    `Email ${email} já está em uso, tente outro email.`,
 };
 
-export const MAIL = {
+export const MAIL = Object.freeze({
   SUBJECT: 'Recuperação de senha',
   TEXT_MAIL: (code: string) =>
     `Você solicitou a recuperação de senha. Aqui está o seu código de recuperação: ${code}`,
-};
+});
